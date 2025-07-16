b2e1d0f30b78a3ccfa8e7fff688e51b6a696c4a0
Wilton Batiquin <cbasher24@gmail.com>
2025-07-16 13:51:56 +0800

    initial commit

---
+++ Class1/src/components/Users.js
@@ -0,0 +1,53 @@
+import { useState, Component } from 'react';
+import User from './User';
+
+import classes from './Users.module.css';
+
+const DUMMY_USERS = [
+  { id: 'u1', name: 'Max' },
+  { id: 'u2', name: 'Manuel' },
+  { id: 'u3', name: 'Julie' },
+];
+
+const Users = () => {
+  const [showUsers, setShowUsers] = useState(true);
+
+  const toggleUsersHandler = () => {
+    setShowUsers((curState) => !curState);
+  };
+
+  class Users extends Component {
+    constructor() {
+      this.state = {
+        showUsers: true
+      };
+    }
+
+    toggleUsersHandler = () => {
+      this.setState({ showUsers: !this.state.showUsers });
+    };
+
+    render() {
+      return (
+        <div className={classes.users}>
+          <button onClick={toggleUsersHandler}>
+            {showUsers ? 'Hide' : 'Show'} Users
+          </button>
+          {showUsers && usersList}
+        </div>
+      );
+    }
+  }
+
+  const usersList = (
+    <ul>
+      {DUMMY_USERS.map((user) => (
+        <User key={user.id} name={user.name} />
+      ))}
+    </ul>
+  );
+
+
+};
+
+export default Users;
